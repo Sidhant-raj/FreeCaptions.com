8d8abc9a4d0f2b2e7f69c5b258770e791bcf951f
Sidhant-raj <2105830@kiit.ac.in>
2024-01-19 23:29:54 +0530

    Added Gunicorn and Requirements.txt

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def upload_file():
 
         return jsonify({"captions": captions})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=False)
